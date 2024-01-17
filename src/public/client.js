f740c14bc4bc6ac42e7a770df11d5a6ddca6ff6f
Julian <ju.french@gmail.com>
2024-01-16 23:15:50 -0600

    Remove comments & cleanup

--- src/public/client.js
+++ src/public/client.js
@@ -1,11 +1,5 @@
-// let store = {
-//     activeRover: { name: "Student" },
-//     apod: '',
-//     rovers: ['Curiosity', 'Opportunity', 'Spirit'],
-// }
-
 let store = Immutable.Map({
-    activeRover: { },
+    activeRover: {},
     rovers: Immutable.List(['curiosity', 'opportunity', 'spirit']),
     rovers_data: Immutable.Map({}),
     apod: {},
@@ -13,59 +7,28 @@ let store = Immutable.Map({
     rovers_ready: false
 });
 
-
-// TODO: 
-/**
- * set active rover after post/get so that the radio button will stay checked
- * push to github
- */
-
-
 // ------------------------------------------------------  
 // STORE MANAGEMENT -------------------------------------
-
-// add our markup to the page
 const root = document.getElementById('root');
 
 const updateStore = (store, newState) => {
-    // store = Object.assign(store, newState)
-    // render(root, store)
-    // const newStore = store.merge(newState);
     const newStore = store.merge(newState);
     render(root, newStore);
-
-    // console.log('%cnewStore:', 'color: #0abab5', newStore.toObject());
 };
-
-// const setStoreData = (_state, _data) => {
-//     console.log('_state:', _state);
-//     console.log('_data:', _data);
-//     store = store.set(_data);
-// };
 
 const setStoreData = (_state, _prop, _data) => {
     store = _state.set(_prop, _data);
-    // const newStore = store.set(_prop, _data);
-
     render(root, store);
-
 };
 
 const render = async (root, state) => {
-    console.log('%cRendering...', 'color: #ed4337');
     root.innerHTML = App(state);
 };
-
-{/* <h2 class="active-rover-title">
-${Object.keys(stateObj.activeRover).length === 0 ? '' : stateObj.activeRover.toObject().name }
-</h2> */}
 
 const App = (state) => {
     let { activeRover, rovers, images, apod } = state.toObject();
     const stateObj = state.toObject();
-    // console.log('%cstateObj:', 'color:orange', stateObj);
-    console.log('%cObject.keys(stateObj.activeRover).length', 'color:orange', Object.keys(stateObj.activeRover).length);
-    // <section class="bg rover-select-wrap>${Object.keys(stateObj.activeRover).length === 0 ? '' : RoverData(activeRover)}</section>
+
     return `
         <header class="bg box header">
             <h1>Mars Rovers Dashboard</h1>
@@ -93,7 +56,7 @@ const App = (state) => {
                 `<section>
                     <!-- ImageOfTheDay(apod) -->
                     <p class="bg box">Image of the day</p>
-                    <div class="image-of-day" style="height: 400px; background-image: url(${ImageOfTheDay(apod)})">
+                    <div class="image-of-day" style="height: 300px; background-image: url(${ImageOfTheDay(apod)})">
                         <a href="${ImageOfTheDay(apod)}" target="_blank" class="click-full-photo">Click here to see full photo</a>
                     </div>
                     <p>${apod.explanation}</p>
@@ -131,7 +94,6 @@ const rover_and_maxDate = (_rover) => {
 };
 
 const activateSlickSlider = () => {
-    // console.log('%cactivateSlickSlider:', 'color: gold', 'Activated');
     $('.images').slick({
         dots: true,
         slidesToShow: 1,
@@ -141,81 +103,25 @@ const activateSlickSlider = () => {
     });
 };
 
-// const mutationSettings = () => {
-//     const target = document.getElementById('root');
-//     const config = { attributes: false, childList: true, subtree: true };
-
-//     const callback = (mutationsList, observer) => {
-//         for(const mutation of mutationsList) {
-//             if (mutation.type === 'childList') {
-//                 const nodes = mutation.addedNodes;
-                
-//                 for (let i = 0; i < nodes.length; i++) {
-//                     if (nodes[i].classList !== undefined && 
-//                         nodes[i].classList.contains('main')) {
-//                         // activateSlickSlider();
-//                     }
-//                 }
-//             }
-//         }
-//         observer.disconnect();
-//     }
-
-//     return {
-//         target,
-//         config,
-//         callback
-//     };
-// };
-// const settingsObj = mutationSettings();
-// const observer = new MutationObserver(settingsObj.callback);
-
 // ------------------------------------------------------ 
 // COMPONENTS -------------------------------------------  
 
-const RoversWrap = (_rovers) => {
-    // return `
-    //     <section class="rovers-wrap">
-    //         ${_rovers.join('')}
-    //     </section>
-    // `
-    return `${_rovers.join('')}`
-};
+const RoversWrap = (_rovers) => `${_rovers.join('')}`;
 
 const ActiveRover = (_state) => {
-    console.log('--- _state ---', _state)
     return `
         <h2 class="active-rover-title">
-        ${Object.keys(_state.activeRover).length === 0 ? '' : _state.activeRover.toObject().name}
+            ${Object.keys(_state.activeRover).length === 0 ? '' : _state.activeRover.toObject().name}
         </h2>
     `
 };
 
 const RoverItem = (state, _rover) => {
-    // const capitalized = _rover.charAt(0).toUpperCase() + _rover.slice(1);
     let activeRover = '';
     if (Object.keys(state.activeRover).length !== 0) {
-        // console.log('RoverItem:', state.activeRover.toObject());
         activeRover = state.activeRover.toObject().name.toLowerCase();
     }
 
-    const isChecked = (_state, __rover) => {
-        let str = '';
-
-        if (Object.keys(state.activeRover).length !== 0) {
-            // console.log('RoverItem:', state.activeRover.toObject());
-            str = state.activeRover.toObject().name.toLowerCase() === __rover ? __rover : '';
-        }
-        // console.log(str + ' is checked!');
-        return str;
-    }
-
-    // return `
-    //     <div class="rover-select">
-    //         <input ${isChecked(state, _rover)} type="radio" id="radio-${_rover}" name="rover" />
-    //         <label class="rover-select-label" for="radio-${_rover}" data-rover="${_rover}">${_rover}</label>
-    //     </div>
-    // `
     return `
         <div class="rover-select">
             <input ${activeRover === _rover ? 'checked' : ''} type="radio" id="radio-${_rover}" name="rover" />
@@ -237,7 +143,6 @@ const RoverData = (_rover) => {
 };
 
 const ImagesWrap = (_images) => {
-    console.log('%c_images:', 'color:dodgerblue', _images)
     return `
         <div class="images">${_images.join('')}</div>
     `
@@ -251,59 +156,42 @@ const Image = (_image) => {
 
 // Example of a pure function that renders infomation requested from the backend
 const ImageOfTheDay = (_apod) => {
-    console.log('%c_apod', 'color:limegreen', _apod)
-    // _apod = _apod.toObject();
+    // If image does not already exist, or it is not from today -- request it again
+    const today = new Date();
 
-        // If image does not already exist, or it is not from today -- request it again
-        const today = new Date();
-        const photodate = new Date(_apod.date);
-        // console.log(photodate.getDate(), today.getDate());
-        // console.log(photodate.getDate() === today.getDate());
-        if (!_apod || _apod.date === today.getDate() ) {
-            // console.log('before getImageOfTheDay:', store);
-            getImageOfTheDay(store);
-        }
+    if (!_apod || _apod.date === today.getDate() ) {
+        getImageOfTheDay(store);
+    }
 
-        if (_apod.hasOwnProperty('error')) {
-            return (`
-                <p style="text-align: center"><span style="color:red">Error: </span><span>${_apod.error.message}</span></p>
-            `)
-        }
+    if (_apod.hasOwnProperty('error')) {
+        return (`
+            <p style="text-align: center"><span style="color:red">Error: </span><span>${_apod.error.message}</span></p>
+        `)
+    }
 
-        // check if the photo of the day is actually type video!
-        if (_apod.media_type === "video") {
-            return (`
-                <p>See today's featured video <a href="${_apod.url}">here</a></p>
-                <p>${_apod.title}</p>
-                <p>${_apod.explanation}</p>
-            `)
-        } else {
-            // <img src="${_apod.url}" height="350px" width="100%" alt="Astrology photo of the day" />
-            // <p>${_apod.explanation}</p>
-            return (`
-                ${_apod.url}
-            `)
-        }
-    // }
+    // check if the photo of the day is actually type video!
+    if (_apod.media_type === "video") {
+        return (`
+            <p>See today's featured video <a href="${_apod.url}">here</a></p>
+            <p>${_apod.title}</p>
+            <p>${_apod.explanation}</p>
+        `)
+    } else {
+        return (`${_apod.url}`)
+    }
 };
 
 // ------------------------------------------------------  
 // API CALLS --------------------------------------------
 
 const getImageOfTheDay = async (state) => {
-    let { apod } = state;
     let data;
     
     try {
         fetch(`http://localhost:3000/apod`)
         .then(res => res.json())
         .then(res => {
-            // updateStore(store, { apod: res.image });
-            console.log('%cgetImageOfTheDay', 'color: gold');
-            // data = res.image;
             setStoreData(store, 'apod', res.image);
-            // updateStore(store, { apod: res.image });
-            // console.log('%cgetImageOfTheDay:', 'color: gold', res);
         });
     } catch (error) {
         console.error(error);
@@ -312,25 +200,7 @@ const getImageOfTheDay = async (state) => {
     return data;
 };
 
-// const postRover = async (_rover) => {
-//     // console.log('%cpost:', 'color: gold', _rover);
-
-//     let fetchRes;
-//     fetchRes = await fetch('http://localhost:3000', {
-//         method: 'POST',
-//         credentials: 'same-origin',
-//         headers: {
-//             'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify({ rover: _rover }),
-//     });
-
-//     return fetchRes;
-// };
-// =============
 const postRover = async (_roverObj) => {
-    // console.log('%cpost:', 'color: gold', _rover);
-
     let fetchRes;
     fetchRes = await fetch('http://localhost:3000', {
         method: 'POST',
@@ -350,9 +220,6 @@ const getRoverImages = async () => {
     try {
         const fetchRes = await fetch('http://localhost:3000/image-data').then(res => res.json());
         data = fetchRes;
-        console.log('%cget images:', 'color: gold', data);
-
-        // updateStore(store, { rovers_data: data });
     } catch (error) {
         console.error(error);
     }
@@ -362,7 +229,6 @@ const getRoverImages = async () => {
 
 const roverInputsReady = (_state) => {
     setStoreData(store, 'rovers_ready', true);
-    console.log('%croverInputsReady:', 'color: gold', 'Ready - Set State');
 };
 
 const getRoverData = async () => {
@@ -372,7 +238,6 @@ const getRoverData = async () => {
         const fetchRes = await fetch('http://localhost:3000/rover-data').then(res => res.json());
         data = fetchRes;
         setStoreData(store, 'rovers_data', data);
-        console.log('%csetStoreRoverData:', 'color: gold', 'Complete');
         roverInputsReady();
     } catch (error) {
         console.error(error);
@@ -381,25 +246,7 @@ const getRoverData = async () => {
     return data;
 };
 
-// const httpRequests_then_updateStore = _rover => {
-//     postRover(_rover)
-//         .then(data => getRoverImages(data))
-//         .then(image_data => image_data.photos.map(obj => obj.img_src))
-//         .then(image_arr => {
-//             const tempObj = {};
-
-//             tempObj.images = image_arr;
-//             tempObj.activeRover = setActiveRoverObject(_rover);
-//             // console.log(tempObj);
-//             // console.log(setActiveRoverObject(_rover));
-//             return tempObj;
-//         })
-//         .then(_tempObj => updateStore(store, _tempObj))
-//         .then(() => activateSlickSlider());
-// };
-// ========
 const httpRequests_then_updateStore = _roverObj => {
-    // console.log('_roverObj:', _roverObj);
     postRover(_roverObj)
         .then(data => getRoverImages(data))
         .then(image_data => image_data.photos.map(obj => obj.img_src))
@@ -420,18 +267,15 @@ const httpRequests_then_updateStore = _roverObj => {
 
 // listening for load event because page should load before any JS is called
 window.addEventListener('load', () => {
-    // observer.observe(settingsObj.target, settingsObj.config);
     getRoverData();
     getImageOfTheDay(store);
     render(root, store);
 });
 
 document.getElementById('body').addEventListener('click', e => {
-
     // * Rover Selection Input Label
     if (e.target.classList.contains('rover-select-label')) {
         const rover = e.target.dataset.rover;
         httpRequests_then_updateStore(rover_and_maxDate(rover));
-        
     }
 });
